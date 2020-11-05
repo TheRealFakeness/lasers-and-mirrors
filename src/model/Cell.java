e70f92b12b7e30e3d39dab42be9e3d4d4cfa45d2
TheRealFakeness <juanjo312003@gmail.com>
2020-11-05 18:16:43 -0500

    Undid stuff for Cell

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -7,6 +7,7 @@ public class Cell {
 	private Cell left;
 	private Cell right;
 	private Cell down;
+	
 	public Cell(Cell up, Cell left, Cell right, Cell down) {
 		this.up = up;
 		this.left = left;
