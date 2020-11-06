e56a3265462dead5d9cb59c6a2d5f72eb3afb496
TheRealFakeness <juanjo312003@gmail.com>
2020-11-05 20:02:12 -0500

    Eclipse is not letting me do anything bruh

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,17 +1,28 @@
 package model;
 
 public class Cell {
-	private String shotDirection;
+	private char mirror;
 	
 	private Cell up;
 	private Cell left;
 	private Cell right;
 	private Cell down;
 	
+	private final static int UP = 1;
+	private final static int RIGHT = 1;
+	private final static int DOWN = 1;
+	private final static int LEFT = 1;
+	
 	public Cell(Cell up, Cell left, Cell right, Cell down) {
 		this.up = up;
 		this.left = left;
 		this.right = right;
 		this.down = down;
+	}
+	
+	public void redirect
+	
+	public String toString() {
+		return "[" + mirror + "]";
 	}
 }
